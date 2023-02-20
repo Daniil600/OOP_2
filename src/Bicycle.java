29e64e85832a8b642965d2cb1d763dd07e2e9e28
Даниил Сазонов <ter6062@ya.ru>
2023-02-20 10:58:00 +0300

    ООП 2 ч.

---
+++ src/Bicycle.java
@@ -0,0 +1,5 @@
+public class Bicycle extends ParentClass{
+    public Bicycle(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+}
---
+++ src/ParentClass.java
@@ -0,0 +1,32 @@
+public abstract class ParentClass {
+    private String modelName;
+    private int wheelsCount;
+
+    public ParentClass(String modelName, int wheelsCount) {
+    }
+
+    public void ParentClass(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void setWheelsCount(int wheelsCount) {
+        this.wheelsCount = wheelsCount;
+    }
+}
