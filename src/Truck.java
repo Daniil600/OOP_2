4d98b38724253ca29d70becf3019b6e77b9c97dd
Sazonov Daniil <ter6062@ya.ru>
2023-02-21 11:30:09 +0300

    ООП 2 ч. 1.12v

--- src/Truck.java
+++ src/Truck.java
@@ -1,8 +1,12 @@
-public class Truck extends Car{
+public class Truck extends Transport{
     public Truck(String modelName, int wheelsCount) {
         super(modelName, wheelsCount);
     }
     public void checkTrailer() {
         System.out.println("Проверяем прицеп");
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
     }
 }
